a0eb780df20f1a5e12a7caaf9e17d17eda4e36ea
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-02-18 22:50:55 -0300

    add todo function

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,7 +4,24 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let todo = document.getElementById('todoAdd').value;
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(), // Remove spaces from the beginning and end of the input string
+      completed: false,
+    };
+
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+      console.log(todos)
+    } else {
+      alert('Please enter a valid todo');
+    }
+    document.getElementById('todoAdd').value = '';
+  }
   
   // Add the deleteToDo code here
 
@@ -18,10 +35,19 @@ const App = () => {
 return(
 <div className ="App">
 <h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
+<form onSubmit={handleSubmit}>
+  <input 
+    type ="text" 
+    align ="right" 
+    id='todoAdd'/>
+  <button 
+    type="submit">Add Todo</button>
 </form>
+{todos.map((todo) => 
+  <div className="todo" key={todo.id}>
+    <div className="todo-text">{todo.text}</div>
+    {/* insert delete button below this line */}
+    </div>)}
 </div>
 );
 };
